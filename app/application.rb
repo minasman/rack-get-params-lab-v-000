e4b392cbc92e30510691beacee0eb8cd59612163
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-30 21:35:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@cart = [] 
+      if @@cart.size == 0  
         resp.write "Your cart is empty"
       else
         @@cart.each {|item| resp.write "#{item}\n"}
